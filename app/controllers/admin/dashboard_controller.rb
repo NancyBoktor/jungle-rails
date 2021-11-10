4a27237af8f6a5ffeefa583c7050195ab2892286
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-10 17:27:31 -0500

    Admin Dashboard Counts.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,7 @@
 class Admin::DashboardController < ApplicationController
   
   def show
-    @category_count = Category.count
-    @product_count = Product.count
-    @total = 0
-    @all_products = Product.all.each do |item|
-      @total += item.quantity
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
